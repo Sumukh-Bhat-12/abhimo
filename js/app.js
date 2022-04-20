1f0a12959a30c33413926aea87a18e0066301f91
Sumukh Bhat <89076392+Sumukh-Bhat-12@users.noreply.github.com>
2022-04-20 18:07:17 +0530

    added more styles and features..

--- js/app.js
+++ js/app.js
@@ -4,14 +4,57 @@ $(document).ready(function () {
     loop: true,
     margin: 0,
     nav: true,
-    smartSpeed:1000,
+    smartSpeed: 1000,
     items: 1,
-    dots:false,
-    navText:['PREV','NEXT'],
+    dots: false,
+    autoplay:false,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    navText: ["PREV", "NEXT"],
     responsive: {
-      0: {},
-      600: {},
-      1000: {},
+      0: {
+        nav: false,
+      },
+      768: {
+        nav: true,
+      },
     },
   });
+
+  //Project Slider
+  $("#project-slider").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    smartSpeed: 1000,
+    dots: false,
+    margin: 24,
+    navText: ["PREV", "NEXT"],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        margin: 0,
+      },
+      768: {
+        items: 2,
+      },
+      1140: {
+        items: 2,
+        center: true,
+        dots: true,
+      },
+    },
+  });
+
+    //Reviews Slider
+    $("#reviews-slider").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      smartSpeed: 800,
+      dots: true,
+      items:1,
+     
+    });
 });
